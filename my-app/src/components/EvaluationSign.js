8124bda022b482edceb317c747c89c7172ba9a77
Nicolas Borboën <ponsfrilus@gmail.com>
2021-03-26 00:15:50 +0100

    [feature] Signature box

--- my-app/src/components/EvaluationSign.js
+++ my-app/src/components/EvaluationSign.js
@@ -8,6 +8,54 @@ class EvaluationSign extends React.Component {
     return(
       <div className={`EvaluationSign`}>
         <h2>Signatures</h2>
+        <div className={`EvaluationSignContainer`}>
+          <div className={`EvaluationSignBlock sign_chef_de_projet`}>
+            <h3>Chef de projet</h3>
+            <p>
+              Lieu :
+              <div className="textLine"></div>
+            </p>
+            <p>
+              Date :
+              <div className="textLine"></div>
+            </p>
+            <p>
+              Signature :
+              <div className="textLine"></div>
+            </p>
+          </div>
+          <div className={`EvaluationSignBlock sign_expert_1`}>
+            <h3>Expert 1</h3>
+            <p>
+              Lieu :
+              <div className="textLine"></div>
+            </p>
+            <p>
+              Date :
+              <div className="textLine"></div>
+            </p>
+            <p>
+              Signature :
+              <div className="textLine"></div>
+            </p>
+          </div>
+          <div className={`EvaluationSignBlock sign_expert_2`}>
+            <h3>Expert 2</h3>
+            <p>
+              Lieu :
+              <div className="textLine"></div>
+            </p>
+            <p>
+              Date :
+              <div className="textLine"></div>
+            </p>
+            <p>
+              Signature 
+              <div className="textLine"></div>
+            </p>
+          </div>
+        </div>
+        
       </div>
     )
   }
