eb9d3dc57b1efebaa540680fff9dac9afe93d3f5
Nicolas Borboën <ponsfrilus@gmail.com>
2021-03-25 23:59:50 +0100

    [feature] grade summary

--- my-app/src/components/EvaluationSummary.js
+++ my-app/src/components/EvaluationSummary.js
@@ -8,6 +8,191 @@ class EvaluationSummary extends React.Component {
     return(
       <div className={`EvaluationSummary`}>
         <h2>Contraction</h2>
+        <div className="divTable">
+          <div className="divTableBody">
+            <div className="divTableRow">
+              <div className="divTableCell">Sommes des différentes parties</div>
+              <div className="divTableCell">Nombre de points max</div>
+              <div className="divTableCell">Points obtenus</div>
+            </div>
+              <div className="divTableRow">
+              <div className="divTableCell">Partie A: Compétences professionnelles</div>
+              <div className="divTableCell">60</div>
+            <div className="divTableCell">&nbsp;</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">Partie B:Documentation / rapport du TPI</div>
+              <div className="divTableCell">30</div>
+              <div className="divTableCell">&nbsp;</div>
+            </div>
+              <div className="divTableRow">
+              <div className="divTableCell">Partie C:Entretien professionnel et présentation</div>
+              <div className="divTableCell">30</div>
+              <div className="divTableCell">&nbsp;</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">Somme de A + B + C</div>
+              <div className="divTableCell">120</div>
+              <div className="divTableCell">&nbsp;</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">Note (5 * somme / 120) + 1</div>
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">&nbsp;</div>
+            </div>
+          </div>
+        </div>
+
+        <p>&nbsp;</p>
+        <div className="divTable">
+          <div className="divTableBody">
+            <div className="divTableRow">
+              <div className="divTableCell tableHeader">Points</div>
+              <div className="divTableCell tableHeader">Note</div>
+              <div className="divTableCell tableHeader">Points</div>
+              <div className="divTableCell tableHeader">Note</div>
+              <div className="divTableCell tableHeader">Points</div>
+              <div className="divTableCell tableHeader">Note</div>
+              <div className="divTableCell tableHeader">Points</div>
+              <div className="divTableCell tableHeader">Note</div>
+              <div className="divTableCell tableHeader">Points</div>
+              <div className="divTableCell tableHeader">Note</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">0 - 1</div>
+              <div className="divTableCell">1.0</div>
+              <div className="divTableCell">23 - 25</div>
+              <div className="divTableCell">2.0</div>
+              <div className="divTableCell">47 - 49</div>
+              <div className="divTableCell">3.0</div>
+              <div className="divTableCell">71 - 73</div>
+              <div className="divTableCell">4.0</div>
+              <div className="divTableCell">95 - 97</div>
+              <div className="divTableCell">5.0</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">2 - 3</div>
+              <div className="divTableCell">1.1</div>
+              <div className="divTableCell">26 - 27</div>
+              <div className="divTableCell">2.1</div>
+              <div className="divTableCell">50 - 51</div>
+              <div className="divTableCell">3.1</div>
+              <div className="divTableCell">74 - 75</div>
+              <div className="divTableCell">4.1</div>
+              <div className="divTableCell">98 - 99</div>
+              <div className="divTableCell">5.1</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">4 - 5</div>
+              <div className="divTableCell">1.2</div>
+              <div className="divTableCell">28 - 29</div>
+              <div className="divTableCell">2.2</div>
+              <div className="divTableCell">52 - 53</div>
+              <div className="divTableCell">3.2</div>
+              <div className="divTableCell">76 - 77</div>
+              <div className="divTableCell">4.2</div>
+              <div className="divTableCell">100 - 101</div>
+              <div className="divTableCell">5.2</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">6 - 8</div>
+              <div className="divTableCell">1.3</div>
+              <div className="divTableCell">30 - 32</div>
+              <div className="divTableCell">2.3</div>
+              <div className="divTableCell">54 - 56</div>
+              <div className="divTableCell">3.3</div>
+              <div className="divTableCell">78 - 80</div>
+              <div className="divTableCell">4.3</div>
+              <div className="divTableCell">102  - 104</div>
+              <div className="divTableCell">5.3</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">9 - 10</div>
+              <div className="divTableCell">1.4</div>
+              <div className="divTableCell">33 - 34</div>
+              <div className="divTableCell">2.4</div>
+              <div className="divTableCell">57 - 58</div>
+              <div className="divTableCell">3.4</div>
+              <div className="divTableCell">81 - 82</div>
+              <div className="divTableCell">4.4</div>
+              <div className="divTableCell">105 - 106</div>
+              <div className="divTableCell">5.4</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">11 - 13</div>
+              <div className="divTableCell">1.5</div>
+              <div className="divTableCell">35 - 37</div>
+              <div className="divTableCell">2.5</div>
+              <div className="divTableCell">59 - 61</div>
+              <div className="divTableCell">3.5</div>
+              <div className="divTableCell">83 - 85</div>
+              <div className="divTableCell">4.5</div>
+              <div className="divTableCell">107 - 109</div>
+              <div className="divTableCell">5.5</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">14 - 15</div>
+              <div className="divTableCell">1.6</div>
+              <div className="divTableCell">38 - 39</div>
+              <div className="divTableCell">2.6</div>
+              <div className="divTableCell">62 - 63</div>
+              <div className="divTableCell">3.6</div>
+              <div className="divTableCell">86 - 87</div>
+              <div className="divTableCell">4.6</div>
+              <div className="divTableCell">110 - 111</div>
+              <div className="divTableCell">5.6</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">16 - 17</div>
+              <div className="divTableCell">1.7</div>
+              <div className="divTableCell">40 - 41</div>
+              <div className="divTableCell">2.7</div>
+              <div className="divTableCell">64 - 65</div>
+              <div className="divTableCell">3.7</div>
+              <div className="divTableCell">88 - 89</div>
+              <div className="divTableCell">4.7</div>
+              <div className="divTableCell">112 - 113</div>
+              <div className="divTableCell">5.7</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">18 - 20</div>
+              <div className="divTableCell">1.8</div>
+              <div className="divTableCell">42 - 44</div>
+              <div className="divTableCell">2.8</div>
+              <div className="divTableCell">66 - 68</div>
+              <div className="divTableCell">3.8</div>
+              <div className="divTableCell">90 - 92</div>
+              <div className="divTableCell">4.8</div>
+              <div className="divTableCell">114 - 116</div>
+              <div className="divTableCell">5.8</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">21 - 22</div>
+              <div className="divTableCell">1.9</div>
+              <div className="divTableCell">45 - 46</div>
+              <div className="divTableCell">2.9</div>
+              <div className="divTableCell">69 - 70</div>
+              <div className="divTableCell">3.9</div>
+              <div className="divTableCell">93 - 94</div>
+              <div className="divTableCell">4.9</div>
+              <div className="divTableCell">117 - 118</div>
+              <div className="divTableCell">5.9</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">&nbsp;</div>
+              <div className="divTableCell">119 - 120</div>
+              <div className="divTableCell">6.0</div>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
