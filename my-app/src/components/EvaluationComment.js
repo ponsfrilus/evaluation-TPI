cf65a040fe0cbca897a0825752f247776141d6ac
Nicolas Borboën <ponsfrilus@gmail.com>
2021-03-26 00:03:24 +0100

    [feature] evaluation comment added

--- my-app/src/components/EvaluationComment.js
+++ my-app/src/components/EvaluationComment.js
@@ -8,6 +8,7 @@ class EvaluationComment extends React.Component {
     return(
       <div className={`EvaluationComment`}>
         <h2>Remarque</h2>
+        <textarea type="text" id={`EvaluationComment_comment`} key={`EvaluationComment_comment`} cols={150} rows={15} ></textarea>
       </div>
     )
   }
