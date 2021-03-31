65ed36c40b0aeec2521cb3a71043d8bb44493324
Nicolas Borboën <ponsfrilus@gmail.com>
2021-03-31 17:02:30 +0200

    [feature] TableContainer for EvaluationSummary

--- src/components/EvaluationSummary.js
+++ src/components/EvaluationSummary.js
@@ -1,45 +1,49 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
+import Paper from '@material-ui/core/Paper';
 import React from 'react'
 class EvaluationSummary extends React.Component {
   render(){
     return(
       <div className={`EvaluationSummary`}>
         <h2>Contraction</h2>
-        <div className="divTable">
-          <div className="divTableHeading">
-            <div className="divTableRow">
-              <div className="divTableHead">Sommes des différentes parties</div>
-              <div className="divTableHead">Nombre de points max</div>
-              <div className="divTableHead">Points obtenus</div>
-            </div>
-          </div>
-          <div className="divTableBody">
-            <div className="divTableRow">
-              <div className="divTableCell">Partie A&thinsp;: Compétences professionnelles</div>
-              <div className="divTableCell tar">60</div>
-              <div className="divTableCell partMarkSum tar" id="partAMarkSum">?</div>
-            </div>
-            <div className="divTableRow">
-              <div className="divTableCell">Partie B&thinsp;: Documentation / rapport du TPI</div>
-              <div className="divTableCell tar">30</div>
-              <div className="divTableCell partMarkSum tar" id="partBMarkSum">&nbsp;</div>
-            </div>
-            <div className="divTableRow">
-              <div className="divTableCell">Partie C&thinsp;: Entretien professionnel et présentation</div>
-              <div className="divTableCell tar">30</div>
-              <div className="divTableCell partMarkSum tar" id="partCMarkSum">&nbsp;</div>
-            </div>
-            <div className="divTableRow">
-              <div className="divTableCell">Somme de A + B + C</div>
-              <div className="divTableCell tar">120</div>
-              <div className="divTableCell tar" id="partTotMarkSum">&nbsp;</div>
-            </div>
-            <div className="divTableRow partFinalGradeRow">
-              <div className="divTableCell partFinalGradeCell"><b>Note</b><br />(5 * somme / 120) + 1</div>
-              <div className="divTableCell partFinalGradeCell" id="partFinGradeNoRound">&nbsp;</div>
-              <div className="divTableCell partFinalGradeCell" id="partFinGrade">&nbsp;</div>
-            </div>
-          </div>
-        </div>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Sommes des différentes parties</TableCell>
+                <TableCell>Nombre de points max</TableCell>
+                <TableCell>Points obtenus</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Partie A&thinsp;: Compétences professionnelles</TableCell>
+                <TableCell className="tar">60</TableCell>
+                <TableCell className="partMarkSum tar" id="partAMarkSum">?</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Partie B&thinsp;: Documentation / rapport du TPI</TableCell>
+                <TableCell className="tar">30</TableCell>
+                <TableCell className="partMarkSum tar" id="partBMarkSum">&nbsp;</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Partie C&thinsp;: Entretien professionnel et présentation</TableCell>
+                <TableCell className="tar">30</TableCell>
+                <TableCell className="partMarkSum tar" id="partCMarkSum">&nbsp;</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Somme de A + B + C</TableCell>
+                <TableCell className="tar">120</TableCell>
+                <TableCell className="tar" id="partTotMarkSum">&nbsp;</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="partFinalGradeCell"><b>Note</b><br />(5 * somme / 120) + 1</TableCell>
+                <TableCell className="partFinalGradeCell" id="partFinGradeNoRound">&nbsp;</TableCell>
+                <TableCell className="partFinalGradeCell" id="partFinGrade">&nbsp;</TableCell>
+              </TableRow>
+            </TableBody>
+            </Table>
+          </TableContainer>
       </div>
     )
   }
