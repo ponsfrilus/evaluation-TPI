18916a90aa391e98a39f0972c4a9797f8dd90c2f
Nicolas Borboën <ponsfrilus@gmail.com>
2021-03-31 17:45:56 +0200

    [fix] margin

--- src/components/EvaluationComment.js
+++ src/components/EvaluationComment.js
@@ -1,21 +1,20 @@
-import { Grid, TextField } from '@material-ui/core'
+import { TextField } from '@material-ui/core'
 import React from 'react'
 class EvaluationComment extends React.Component {
   render() {
     return (
       <div className={`EvaluationComment`}>
         <h2>Remarques</h2>
-          <TextField
-            id="EvaluationComment_comment"
-            style={{ margin: 8 }}
-            placeholder="Inscrivez ici toutes remarques concernant la procédure de qualification..."
-            fullWidth
-            multiline
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
+        <TextField
+          id="EvaluationComment_comment"
+          placeholder="Inscrivez ici toutes remarques concernant la procédure de qualification..."
+          fullWidth
+          multiline
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
         />
       </div>
     )
