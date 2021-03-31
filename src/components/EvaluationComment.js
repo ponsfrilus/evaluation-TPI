e07455fa70d70c901ec189e826909fb95975afd5
Nicolas Borboën <ponsfrilus@gmail.com>
2021-03-31 17:43:24 +0200

    [feature] use of TextField for evaluationComment

--- src/components/EvaluationComment.js
+++ src/components/EvaluationComment.js
@@ -1,15 +1,22 @@
+import { Grid, TextField } from '@material-ui/core'
 import React from 'react'
 class EvaluationComment extends React.Component {
   render() {
     return (
       <div className={`EvaluationComment`}>
         <h2>Remarques</h2>
-        <textarea
-          id={`EvaluationComment_comment`}
-          key={`EvaluationComment_comment`}
-          cols={100}
-          rows={15}
-        ></textarea>
+          <TextField
+            id="EvaluationComment_comment"
+            style={{ margin: 8 }}
+            placeholder="Inscrivez ici toutes remarques concernant la procédure de qualification..."
+            fullWidth
+            multiline
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="outlined"
+        />
       </div>
     )
   }
